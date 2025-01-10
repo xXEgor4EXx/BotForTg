c3537c5d8d56d10ad95a8d14fcf89e33c190ebc2
xXEgor4EXx <egor4eeex@mail.ru>
2025-01-10 11:08:36 +0500

    message

---
+++ botfordoor.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from Config import TOKEN
+
+from app.handlers import router
+from app.database.models import async_main
+
+async def main():
+    await async_main()
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот   выключен')
